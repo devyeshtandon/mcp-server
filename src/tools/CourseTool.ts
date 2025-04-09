5625d4fbba522c396dca008c6d636d7aba684252
devyeshtandon <devyeshtandon@gmail.com>
2025-04-09 15:10:17 +0530

    CoursesTool init

---
+++ src/tools/CourseTool.ts
@@ -0,0 +1,6 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { getCourseById, getCourses } from "../api/courses.js";
+import { z } from "zod";
+
+export class CoursesTool {
+}
