43172be0e9dfe558722141db343852f7422918f8
devyeshtandon <devyeshtandon@gmail.com>
2025-04-09 18:21:00 +0530

    get all blog posts

--- src/api/blogs.ts
+++ src/api/blogs.ts
@@ -4,5 +4,25 @@ export class BlogPosts {
     private static readonly baseUrl: string = "https://interviewready.io/_nuxt/content/db-96806b65.json";
     private static cachedResponse: ReducedItemData[] | null = null;
 
+
+    static async getAllBlogPosts() {
+        if (this.cachedResponse) {
+            return this.cachedResponse;
+        }
+
+        const response = await fetch(this.baseUrl);
+        const data: BlogResponse = await response.json();
+        const blogData = data._collections[0]._data;
+        const reducedBlogData: ReducedItemData[] = blogData.map((post) => ({
+            slug: post.slug,
+            title: post.title,
+            keywords: post.keywords || post.Keywords,
+            bodyPlainText: post.bodyPlainText,
+        }));
+
+        this.cachedResponse = reducedBlogData;
+        return reducedBlogData;
+    }
+
 }
 
