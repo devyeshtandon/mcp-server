ec6a3c68872b1b2757c60552d7d453ff02105f6d
devyeshtandon <devyeshtandon@gmail.com>
2025-04-09 15:11:10 +0530

    registerTool get courses

--- src/tools/CourseTool.ts
+++ src/tools/CourseTool.ts
@@ -8,4 +8,33 @@ export class CoursesTool {
         this.registerTool();
     }
 
+    private registerTool() {
+        this.server.tool(
+            "get-courses",
+            "Get all courses on interviewready platform",
+            {},
+            async () => this.getCourses(),
+        );
+
+    }
+
+    async getCourses() {
+        const courses = await getCourses();
+        const relevantData = courses.map((course) => ({
+            id: course.id,
+            name: course.name,
+            description: course.description,
+            thumbnail_url: course.thumbnail_url,
+        }));
+
+        return {
+            content: [
+                {
+                    type: "text" as const,
+                    text: JSON.stringify(relevantData),
+                },
+            ],
+        };
+    }
+
 }
