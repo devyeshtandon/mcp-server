c6f61624d872388e2c96e34c0fd43bf9c7244c17
devyeshtandon <devyeshtandon@gmail.com>
2025-04-09 15:29:33 +0530

    endpoint corrected

--- src/api/courses.ts
+++ src/api/courses.ts
@@ -1,7 +1,7 @@
 import { CourseResponse, CoursesWithStatsResponse } from "../types/api/courses.js"
 
 export async function getCourses(): Promise<CourseResponse[]> {
-    const API = "https://api.interviewready.io/api/v1/courses/1"
+    const API = "https://api.interviewready.io/api/v1/courses"
 
     const response = await fetch(API)
     if (!response.ok) {
