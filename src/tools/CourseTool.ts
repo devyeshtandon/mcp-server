3d7d3c65a0ca3534afce949ef11a3531080bb0ad
devyeshtandon <devyeshtandon@gmail.com>
2025-04-09 15:10:40 +0530

    init/2

--- src/tools/CourseTool.ts
+++ src/tools/CourseTool.ts
@@ -3,4 +3,9 @@ import { getCourseById, getCourses } from "../api/courses.js";
 import { z } from "zod";
 
 export class CoursesTool {
+    constructor(private readonly server: McpServer) {
+        this.server = server;
+        this.registerTool();
+    }
+
 }
